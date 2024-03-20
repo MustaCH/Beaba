e8a3e20c2447172f2814cdd6688916e8d3fd8041
Ignacio Nicolás García Poletti <polettiignacio7@gmail.com>
2024-03-20 20:12:30 -0300

    modified carousel slide

---
+++ pages/project-detail/carousel.js
@@ -0,0 +1,43 @@
+//Carousel proyecto
+
+document.addEventListener("DOMContentLoaded", function () {
+  const slides = document.querySelectorAll(".slide");
+  let currentSlideIndex = 0;
+
+  function showNextSlide() {
+    slides[currentSlideIndex].classList.remove("z-50");
+    currentSlideIndex = (currentSlideIndex + 1) % slides.length;
+    slides[currentSlideIndex].classList.add("z-50");
+  }
+
+  function showPreviousSlide() {
+    slides[currentSlideIndex].classList.remove("z-50");
+    currentSlideIndex = (currentSlideIndex - 1 + slides.length) % slides.length;
+    slides[currentSlideIndex].classList.add("z-50");
+  }
+
+  function handleTouchStart(event) {
+    startX = event.touches[0].clientX;
+  }
+
+  function handleTouchMove(event) {
+    if (!startX) return;
+    const currentX = event.touches[0].clientX;
+    const diff = startX - currentX;
+    if (diff > 0) {
+      showNextSlide();
+    } else {
+      showPreviousSlide();
+    }
+    startX = null;
+  }
+
+  let startX;
+
+  document
+    .querySelector("#carousel")
+    .addEventListener("touchstart", handleTouchStart);
+  document
+    .querySelector("#carousel")
+    .addEventListener("touchmove", handleTouchMove);
+});
