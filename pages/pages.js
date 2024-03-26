437cb474ff1557a0894cb17733fb5f60f7fcfbcc
Ignacio Nicolás García Poletti <polettiignacio7@gmail.com>
2024-03-26 17:58:56 -0300

    optimized picture sizes

--- pages/pages.js
+++ pages/pages.js
@@ -15,30 +15,3 @@ const CloseSidebar = () => {
   toggleMenu.classList.remove("opacity-0");
   closeMenu.classList.add("hidden");
 };
-
-//COPY TO CLIPBOARD
-
-let textCopied = document.getElementById("phone");
-
-const CopyToClipBoard = () => {
-  textCopied.addEventListener("click", function (event) {
-    event.preventDefault();
-
-    let textToCopy = textCopied.innerText;
-
-    navigator.clipboard
-      .writeText(textToCopy)
-      .then(function () {
-        let tooltip = document.getElementById("tooltip");
-        tooltip.classList.remove("hidden");
-        setTimeout(() => {
-          tooltip.classList.add("hidden");
-        }, 2000);
-      })
-      .catch(function (err) {
-        console.error("Error al intentar copiar el texto: ", err);
-      });
-  });
-};
-
-CopyToClipBoard();
